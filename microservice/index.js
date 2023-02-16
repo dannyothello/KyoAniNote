47541f8bebe238f0167280c94e078d8776c8eb11
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-02-16 12:57:36 +0900

    Set up microservice file directory.

---
+++ microservice/index.js
@@ -0,0 +1,6 @@
+// This microservice receives:
+// A list containing JSON objects with keys of "title" and "visual".
+// 
+// This microservice returns:
+// A list containing 5 randomly picked JSON objects.
+
---
+++ microservice/sampleClientCode.js
@@ -0,0 +1,2 @@
+// This is some example code, which shows how to implement
+// this microservice in a client.
