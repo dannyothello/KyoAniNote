f3242e38024ad75c22f7e107ebdbb32f54ca94cb
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-02-16 13:07:31 +0900

    Launched hello world server with express

--- microservice/index.js
+++ microservice/index.js
@@ -4,3 +4,21 @@
 // This microservice returns:
 // A list containing 5 randomly picked JSON objects.
 
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const PORT = 3001
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Microservice is running, listening on port "+ PORT)
+	else
+		console.log("Error occurred, microservice can't start", error)
+	}
+)
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+})
